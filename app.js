56f656efcee071c9511c4d49c04d6c04975fab8e
edurekalearning <prerna.kumari@edureka.co>
2026-05-19 17:28:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// const express = require("express");
+// const userRoutes = require("./routes/userRoutes");
+// const requestLogger = require("./middleware/requestLogger");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(requestLogger);
+
+// app.use("/api", userRoutes);
+
+// module.exports = app;
+
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+const requestLogger = require("./middleware/requestLogger");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.use("/api", userRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
